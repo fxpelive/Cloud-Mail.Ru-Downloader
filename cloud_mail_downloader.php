3f3dab92fc17d1df7d6a0739882f9ad61a0eb1c6
fxpelive <fxpelive@users.noreply.github.com>
2023-06-16 17:29:44 +0500

    Update cloud_mail_downloader.php

--- cloud_mail_downloader.php
+++ cloud_mail_downloader.php
@@ -17,22 +17,36 @@
 	$aria2c = pathcombine($current_dir, "aria2c");
 	$links = file($links_file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
-	echo "Start create input file for Aria2c Downloader..." . PHP_EOL;
+	
 	foreach($links as $link)
 	{
 		$link = trim($link);
 		if (strpos($link, 'http') !== 0) continue;
 
-		$file4aria = pathcombine($current_dir, "input" . time() . ".txt");
-		if (file_exists($file4aria)) unlink($file4aria);
 
-		if (!preg_match('~/public/([^/]+/[^/]+)~', $link, $match)) die("Wrong link " . $link . PHP_EOL);
+		if (!preg_match('~/public/([^/]+/[^/]+)~', $link, $match)) {
+		//die("Wrong link " . $link . PHP_EOL);
+		echo "Wrong link " . $link . PHP_EOL;
+		continue;
+		}
 		$link_id = $match[1];
 		$page_id = GetPageId($link);
-		if (!$page_id) die("Page ID not found " . $link . PHP_EOL);
+		if (!$page_id) {
+		//die("Page ID not found " . $link . PHP_EOL);
+		echo "Page ID not found " . $link . PHP_EOL;
+		continue;
+		}
 		$base_url = GetBaseUrl($page_id);
-		if (!$base_url) die("Can't get base URL " . $link . PHP_EOL);
-
+		if (!$base_url) {
+		//die("Can't get base URL " . $link . PHP_EOL);
+		echo "Can't get base URL " . $link . PHP_EOL;
+		continue;
+		}
+		
+		echo "Start create input file for Aria2c Downloader..." . PHP_EOL;
+		$file4aria = pathcombine($current_dir, "input" . time() . ".txt");
+		if (file_exists($file4aria)) unlink($file4aria);
+		
 		if ($files = GetAllFiles($link_id))
 		{
 			foreach ($files as $file)
@@ -48,7 +62,10 @@
 		}
 		else
 		{
-			die("Can't find any file" . PHP_EOL);
+			//die("Can't find any file" . PHP_EOL);
+			echo "Can't find any file" . PHP_EOL;
+			continue;
+			
 		}
 	}
 	echo "Done!" . PHP_EOL;
@@ -73,6 +90,34 @@
 		global $main_url, $base_url, $page_id;
 
 		$json = json_decode(get("{$main_url}folder?weblink=" . $link . "&x-page-id={$page_id}"), true);
+		
+		//---
+		$need_all=($json["body"]['count']['folders'] + $json["body"]['count']['files']) >=500 ;
+		if(!true   or $need_all ) {
+		//file_put_contents("R:\json_std.json", json_encode($json,true) );
+		$offset=0;
+		$limit=50;
+		$json=null;
+		$items_list=array();
+		while(true){
+		$params=array('offset' => $offset, 'limit' => $limit);
+		$queryParams = http_build_query($params);
+		$json_obj = json_decode(get("{$main_url}folder?weblink=" . $link . "&x-page-id={$page_id}". "&".$queryParams), true);
+		if(null==$json_obj){ print_r("{$main_url}folder?".$queryParams."\n"); exit("Smth went wrong..."); }
+		$count_=$json_obj["body"]['count']['folders'] + $json_obj["body"]['count']['files'];
+		$items=$json_obj["body"]["list"];
+		$items_list=array_merge($items_list,$items);
+		if(is_null($json)){ $json=$json_obj; }
+		if ( ($offset > $count_) or  (count($items) == 0)  or ($json_obj["status"]!=200) ){ break; }
+		echo $offset.":".$limit."\n";
+		
+		$offset += $limit;
+		}
+		$json["body"]["list"]=$items_list;
+		//file_put_contents("R:\json_all.json", json_encode($json,true) );
+		//print_r($json);exit;
+		}
+		//---
 
 		$cmfiles = array();
 		if (!isset($json["body"]["list"])) return $cmfiles;
@@ -101,7 +146,7 @@
 	{
 		global $aria2c, $file4aria, $proxy, $proxy_auth;
 
-		$command = "\"$aria2c\" --file-allocation=none --max-connection-per-server=10 --split=10 --max-concurrent-downloads=10 --summary-interval=0 --continue --user-agent=\"Mozilla/5.0 (compatible; Firefox/3.6; Linux)\" --input-file=\"$file4aria\"";
+		$command = "\"$aria2c\"   --max-tries=100 --retry-wait=1 --file-allocation=none --max-connection-per-server=16 --split=16 --min-split-size=1M --max-concurrent-downloads=4 --summary-interval=0 --continue --user-agent=\"Mozilla/5.0 (compatible; Firefox/3.6; Linux)\" --input-file=\"$file4aria\"";
 		if ($proxy) $command .= " --all-proxy=" . ($proxy_auth ? "$proxy_auth@" : "") . "$proxy";
 		passthru("{$command}");
 	}
@@ -111,6 +156,7 @@
 	function GetPageId($url)
 	{
 		$page = get($url);
+		//if (preg_match('~"pageId":"([^"]+)"~s', $page, $match)) return $match[1];
 		if (preg_match('~pageId[\'"]*:[\'"]*([^"\'\s,]+)~s', $page, $match)) return $match[1];
 		return false;
 	}
